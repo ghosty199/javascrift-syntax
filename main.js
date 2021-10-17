23e1627ff98db058c62b314c2078c6bc09a99320
ghosty199 <88465104+ghosty199@users.noreply.github.com>
2021-10-17 09:27:19 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+var firstname="levi"
+var secondname="ackerman"
+var fullname=firstname.concat(secondname)
+console.log(fullname)
+console.log(fullname.length)
+var name="kim namjoon"
+console.log(name.replace("kim","ben"))
+console.log(name.toUpperCase())
+var squareroot=Math.sqrt(23)
+    console.log(squareroot)
+var max=Math.max(10,20,30,40,50)
+console.log(max)
+var min=Math.min(10,20,30,40,50)
+console.log(min)
+var power=Math.pow(4,5)
+console.log(power)
+console.log(squareroot.toFixed(2))
+var number1=10
+var number2=20
+var num1=number1.toString()
+var num2=number2.toString()
+console.log(num1+num2)
+console.log(typeof number1)
+console.log(typeof num1)
+console.log(number1+number2)
+console.log(number1++)
+console.log(number2--)
+
+
+
+
+
+
+
